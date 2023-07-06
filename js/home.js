afbf66f4d777fcf8fb5391f46c3e15a58bcebc03
ngochoatran <nlsngochoa@gmail.com>
2023-07-06 18:21:59 +0700

    Cập nhật js/home.js và views/home.view.php

--- js/home.js
+++ js/home.js
@@ -1,4 +1,9 @@
 const btnLogout = document.querySelector('button')
+const btnAdd = document.getElementById('add-btn');
+const btnClose = document.getElementById('close-btn');
+const popupForm = document.getElementById('popup-form');
+const addBookForm = document.getElementById('add-book-form');
+
 btnLogout.addEventListener('click', async () => {
   try {
     const response = await Requester.delete('/logout', {
@@ -18,3 +23,117 @@ btnLogout.addEventListener('click', async () => {
     }
   }
 })
+
+btnAdd.addEventListener('click', () => {
+  popupForm.style.display = 'block'; // Display the popup form
+});
+
+btnClose.addEventListener('click', () => {
+  popupForm.style.display = 'none'; // Hide the popup form
+});
+
+addBookForm.addEventListener('submit', async (e) => {
+  e.preventDefault(); // Prevent form submission
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const publishedYear = document.getElementById('published-year').value;
+  const price = document.getElementById('price').value;
+
+  try {
+    const response = await Requester.post('/php-intro/books', {
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        title: title,
+        author: author,
+        published_year: publishedYear,
+        price: price
+      })
+    });
+
+    if (!response.ok) {
+      const result = await response.json();
+      window.alert(result.message);
+    } else {
+      // Refresh the book table
+     window.location.reload();
+    }
+  } catch (error) {
+    window.alert(error.message);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Lấy danh sách sách từ API và hiển thị trên trang
+  fetch('/php-intro/books')
+      .then(response => response.json())
+      .then(data => {
+          // Lưu thông tin sách vào đối tượng booksObject
+          const booksObject = {};
+          data.forEach(book => {
+              const bookInfo = {
+                  id: book.id,
+                  title: book.title,
+                  author: book.author,
+                  published_year: book.published_year,
+                  price: book.price,
+                  user_id: book.user_id,
+                  created_at: new Date(book.created_at),
+                  updated_at: new Date(book.updated_at)
+              };
+              booksObject[book.id] = bookInfo;
+          });
+
+          // Hiển thị thông tin sách từ đối tượng booksObject
+          displayBooks(booksObject);
+      })
+      .catch(error => {
+          console.error('Error:', error);
+      });
+});
+
+function displayBooks(booksObject) {
+  const tableBody = document.getElementById('book-table-body');
+  tableBody.innerHTML = '';
+
+  for (const bookId in booksObject) {
+      if (booksObject.hasOwnProperty(bookId)) {
+          const book = booksObject[bookId];
+
+          const row = document.createElement('tr');
+
+          const idCell = document.createElement('td');
+          idCell.textContent = book.id;
+          row.appendChild(idCell);
+
+          const titleCell = document.createElement('td');
+          titleCell.textContent = book.title;
+          row.appendChild(titleCell);
+
+          const authorCell = document.createElement('td');
+          authorCell.textContent = book.author;
+          row.appendChild(authorCell);
+
+          const publishedYearCell = document.createElement('td');
+          publishedYearCell.textContent = book.published_year;
+          row.appendChild(publishedYearCell);
+
+          const priceCell = document.createElement('td');
+          priceCell.textContent = book.price;
+          row.appendChild(priceCell);
+
+          const createdCell = document.createElement('td');
+          createdCell.textContent = book.created_at.toLocaleString();
+          row.appendChild(createdCell);
+
+          const updatedCell = document.createElement('td');
+          updatedCell.textContent = book.updated_at.toLocaleString();
+          row.appendChild(updatedCell);
+
+          tableBody.appendChild(row);
+      }
+  }
+}
--- views/home.view.php
+++ views/home.view.php
@@ -6,10 +6,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="./js/requester.js"></script>
     <title>Home</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-bottom: 20px;
+            margin-top: 20px;
+            background-color: #fff;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+        }
+
+        th,
+        td {
+            padding: 10px;
+            text-align: left;
+            border-bottom: 1px solid #ddd;
+        }
+    </style>
 </head>
 
 <body>
-    <button type="button">Logout</button>
+    <button id="logout-btn" type="button">Logout</button>
+    <button id="add-btn">Add</button>
+
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Author</th>
+                <th>Published Year</th>
+                <th>Price</th>
+                <th>Created At</th>
+                <th>Updated At</th>
+            </tr>
+        </thead>
+        <tbody id="book-table-body"></tbody>
+    </table>
+
+    <!-- Popup form -->
+    <div id="popup-form" style="display: none;">
+        <h2>Add Book</h2>
+        <form id="add-book-form">
+            <label for="title">Title:</label>
+            <input type="text" id="title" name="title" required><br><br>
+            <label for="author">Author:</label>
+            <input type="text" id="author" name="author" required><br><br>
+            <label for="published-year">Published Year:</label>
+            <input type="number" id="published-year" name="published-year" required><br><br>
+            <label for="price">Price:</label>
+            <input type="number" id="price" name="price" required><br><br>
+            <button type="submit">Add</button>
+            <button type="button" id="close-btn">Close</button>
+        </form>
+    </div>
+
     <script src="./js/home.js"></script>
 </body>
 
